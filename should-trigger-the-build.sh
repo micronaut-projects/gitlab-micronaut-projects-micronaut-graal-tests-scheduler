4a52aebd4e770741df97611ff84578899e04982b
Iván López <lopez.ivan@gmail.com>
2019-05-02 16:30:23 +0200

    Use Micronaut graal-rc17 branch

--- should-trigger-the-build.sh
+++ should-trigger-the-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MICRONAUT_BRANCH=master
+MICRONAUT_BRANCH=graal-rc17
 
 curl -s https://api.github.com/repos/oracle/graal/commits/master | jq -r .sha > .tmp_graal
 curl -s https://api.github.com/repos/micronaut-projects/micronaut-core/commits/$MICRONAUT_BRANCH | jq -r .sha > .tmp_micronaut
