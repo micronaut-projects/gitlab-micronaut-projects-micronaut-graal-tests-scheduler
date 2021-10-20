83bb2bef314f161b6fb9aacb08f30d95305cac67
Iván López <lopezi@objectcomputing.com>
2021-10-20 10:19:25 +0200

    Micronaut branch is 3.2.x

--- should-trigger-the-build.sh
+++ should-trigger-the-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MICRONAUT_BRANCH=3.1.x
+MICRONAUT_BRANCH=3.2.x
 
 curl -s https://api.github.com/repos/micronaut-projects/micronaut-core/commits/$MICRONAUT_BRANCH | jq -r .sha > .tmp_micronaut
 
