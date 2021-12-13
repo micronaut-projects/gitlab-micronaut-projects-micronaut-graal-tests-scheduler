f4a0f42797039e7177ebf1b3c119aa5f22fde67a
Iván López <lopezi@objectcomputing.com>
2021-12-13 11:16:08 +0100

    GraalVM branch is 'release/graal-vm/22.0'

--- should-trigger-the-build.sh
+++ should-trigger-the-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MICRONAUT_BRANCH=3.3.x
-GRAALVM_BRANCH="master"
+GRAALVM_BRANCH="release/graal-vm/22.0"
 
 curl -s https://api.github.com/repos/oracle/graal/commits/$GRAALVM_BRANCH | jq -r .sha > .tmp_graalvm
 curl -s https://api.github.com/repos/micronaut-projects/micronaut-core/commits/$MICRONAUT_BRANCH | jq -r .sha > .tmp_micronaut
