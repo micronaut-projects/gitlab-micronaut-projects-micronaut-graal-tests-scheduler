68fdf41806035f6908557f786272d0c0e1178c45
Sergio del Amo <sergio.delamo@softamo.com>
2023-04-17 21:35:58 +0200

    [ci skip] Micronaut Branch is 3.10.x

--- should-trigger-the-build.sh
+++ should-trigger-the-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MICRONAUT_BRANCH=3.9.x
+MICRONAUT_BRANCH=3.10.x
 
 curl -s https://api.github.com/repos/micronaut-projects/micronaut-core/commits/$MICRONAUT_BRANCH | jq -r .sha > .tmp_micronaut
 
